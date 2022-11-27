8a1380aea9857bb0b49284c245844ef5d0c9e398
jaydon <jaydon.devandpm@gmail.com>
2022-11-27 23:04:18 +0800

    修改app启动白屏问题

--- app/src/main/java/com/jaydon/listenbooks/SplashActivity.java
+++ app/src/main/java/com/jaydon/listenbooks/SplashActivity.java
@@ -3,23 +3,34 @@ package com.jaydon.listenbooks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import me.wangyuwei.particleview.ParticleView;
+
 public class SplashActivity extends AppCompatActivity {
+    ParticleView mParticview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        mParticview = (ParticleView) findViewById(R.id.particlview);
+        mParticview.postDelayed(new Runnable() {
             @Override
             public void run() {
+                mParticview.startAnim();
+            }
+        }, 300);
+        mParticview.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        }, 3000);
+        });
+
     }
 }
