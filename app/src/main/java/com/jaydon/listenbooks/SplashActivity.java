3466ff55a7d85ffc24b14cd8059c60e92eabb779
JaydonLi <jaydon.devandpm@gmail.com>
2022-11-28 15:45:22 +0800

    修改splash

--- app/src/main/java/com/jaydon/listenbooks/SplashActivity.java
+++ app/src/main/java/com/jaydon/listenbooks/SplashActivity.java
@@ -1,36 +1,34 @@
 package com.jaydon.listenbooks;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import me.wangyuwei.particleview.ParticleView;
 
 public class SplashActivity extends AppCompatActivity {
-    ParticleView mParticview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        mParticview = (ParticleView) findViewById(R.id.particlview);
-        mParticview.postDelayed(new Runnable() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                mParticview.startAnim();
-            }
-        }, 300);
-        mParticview.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
-            @Override
-            public void onAnimationEnd() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+
             }
-        });
+        }, 500);
+
 
     }
 }
