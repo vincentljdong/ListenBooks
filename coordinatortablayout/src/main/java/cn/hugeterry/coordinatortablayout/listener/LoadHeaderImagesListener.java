304a37f604896cecd8a10f4266d416b33e25d077
jaydon <jaydon.devandpm@gmail.com>
2022-12-03 14:00:02 +0800

    添加tab框架

---
+++ coordinatortablayout/src/main/java/cn/hugeterry/coordinatortablayout/listener/LoadHeaderImagesListener.java
@@ -0,0 +1,14 @@
+package cn.hugeterry.coordinatortablayout.listener;
+
+
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
+
+/**
+ * @author hugeterry(http://hugeterry.cn)
+ */
+
+public interface LoadHeaderImagesListener {
+    void loadHeaderImages(ImageView imageView, TabLayout.Tab tab);
+}
